212a5b636c8c31ab19d956e81f7b86425a0e8e2d
Guobao Shen <shengb@bnl.gov>
2014-09-18 11:24:56 -0400

    add epics db files for id local bump

---
+++ iocBoot/ioclocalbump/st.cmd
@@ -0,0 +1,19 @@
+#!../../bin/darwin-x86/dms4ap
+
+## You may have to change betafunc to something else
+## everywhere it appears in this file
+
+< envPaths
+
+cd ${TOP}
+
+## Register all support components
+dbLoadDatabase "dbd/dms4ap.dbd"
+dms4ap_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords "db/idlocalbump.db"
+dbLoadRecords "iocsim/bpm_cor.db"
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
